327fe90a9595c2f0204327056a752e573d8d8a41
Adrian Corcoran <adrian.corcoran@shopify.com>
2019-07-07 21:47:10 +0100

    Complete Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { setContext } from "apollo-link-context";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const credentials = require("./credentials");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql"
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = credentials.GITHUB_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
